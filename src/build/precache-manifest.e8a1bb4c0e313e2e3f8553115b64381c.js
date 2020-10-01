6c03b2c3834a5685205c0794eeb24c2b2d1085ec
Stephen Blair <stephenjohnblair@outlook.com>
2020-10-01 11:11:43 +0100

    Created front end build folder from React-Blog and copied directory to backend. Added paths and routes

---
+++ src/build/precache-manifest.e8a1bb4c0e313e2e3f8553115b64381c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8b892fa18745e18eb591ad35689d1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8de6f1e78260c581934f",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a13931dea87f0c418e43",
+    "url": "/static/js/2.719d26ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.719d26ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de6f1e78260c581934f",
+    "url": "/static/js/main.9bb2e431.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  }
+]);
